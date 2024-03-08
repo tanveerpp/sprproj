7770a8f76eddb3f043a82f0e10ff3577e9e38e4d
tanveerpp <tanveerprogrammerspoint@gmail.com>
2024-03-08 19:37:53 +0530

    first commit

---
+++ src/main/java/com/example/demo/PpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpointApplication.class, args);
+	}
+
+}
